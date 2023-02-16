1d1afba25116a11a9a47e4cecc849420850339b2
Ivan Ignacio Peña Mattos <76266733+IP-Mattos@users.noreply.github.com>
2023-02-16 00:54:29 -0300

    refactor: Refactts components

--- src/app/components/tab/tab.component.ts
+++ src/app/components/tab/tab.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Toggle } from '../../../util/Toggle';
 
 @Component({
   selector: 'app-tab',
   templateUrl: './tab.component.html',
-  styleUrls: ['./tab.component.css']
+  styleUrls: ['./tab.component.css'],
 })
 export class TabComponent {
+  constructor(public toggle: Toggle) {}
 
+  OnToggle() {
+    this.toggle.state = !this.toggle.state;
+  }
 }
