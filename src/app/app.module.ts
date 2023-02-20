c1a0ad711124ea23a9c8d0cf22c4105e8bd85f7c
Ivan Ignacio Peña Mattos <76266733+IP-Mattos@users.noreply.github.com>
2023-02-20 13:48:33 -0300

    Styles windows zp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { WindowComponent } from './components/window/window.component';
 import { TaskbarComponent } from './components/taskbar/taskbar.component';
 import { TabComponent } from './components/tab/tab.component';
+import { FolderComponent } from './components/folder/folder.component';
 
 @NgModule({
-  declarations: [AppComponent, WindowComponent, TaskbarComponent, TabComponent],
+  declarations: [AppComponent, WindowComponent, TaskbarComponent, TabComponent, FolderComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
