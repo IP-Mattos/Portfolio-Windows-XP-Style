ee39581cde71fc07f3b170438bb445a79e712c47
Ivan Ignacio Peña Mattos <76266733+IP-Mattos@users.noreply.github.com>
2023-02-22 19:11:18 -0300

    Update tab.component.ts

--- src/app/components/tab/tab.component.ts
+++ src/app/components/tab/tab.component.ts
@@ -8,11 +8,11 @@ import { Toggle } from '../../../util/Toggle';
 })
 export class TabComponent {
   constructor(public toggle: Toggle) {}
-  Tittle = 'Bienvenidos a la Replica de Windows XP/Porfolio';
+  Tittle = 'Bienvenidos a la Réplica de Windows XP/Porfolio';
 
   Description = 'Soy Iván Peña FullStack Developer';
   Guide =
-    'Para poder navegar por la replica puedes abrir las diferentes secciones haciendo doble click en los iconos del escritorio y si clickeas en el botón de inicio podrás ver más información sobre mi!';
+    'Para poder navegar por la réplica puedes abrir las diferentes secciones haciendo doble click en los iconos del escritorio y si clickeas en el botón de inicio podrás ver más información sobre mí!';
   OnToggle() {
     this.toggle.state = !this.toggle.state;
   }
